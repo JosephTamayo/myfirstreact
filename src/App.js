ce3e1f745b6f5a5781cc34a477a8d1c90b6ba7a7
JosephTamayo <josephgabriel.tamayo.iics@ust.edu.ph>
2021-11-10 14:54:55 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> HATDOG.
+          Edit <code>src/App.js</code> pogi ni sir.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          cick this link :) 
         </a>
       </header>
     </div>
