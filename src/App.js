67d9f8d5b09305c55d346f1ae133729eb72bbcb7
JosephTamayo <josephgabriel.tamayo.iics@ust.edu.ph>
2021-11-10 14:56:01 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          cick this link :) 
+          click this link :) 
         </a>
       </header>
     </div>
