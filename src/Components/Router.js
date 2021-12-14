5d7484c36a1232ebfe57ad7a9002d511b3a19ca5
JosephTamayo <josephgabriel.tamayo.iics@ust.edu.ph>
2021-12-14 21:54:06 +0800

    update

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -7,7 +7,7 @@ import AddEmployee from './AddEmployee';
 const Router = () =>{
     return(
         <div>
-            <BrowserRouter basename="/index.html">
+            <BrowserRouter basename="/">
             <Routes>
                 <Route exact path = "/myfirstreact" element = {<Home/>}/>
                 <Route exact path = "/myfirstreact/employees" element = {<Employee/>}/>
